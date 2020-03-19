dbf63861a0d2c548f88880f2730287ea5d260d1c
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-19 10:53:34 +0800

    Updates

---
+++ precache-manifest.cc49d59c57032e29bfc8401b37d5edce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13b95454e792a23ca889",
+    "url": "/react-lights-out/static/css/main.9b9d9414.chunk.css"
+  },
+  {
+    "revision": "13b95454e792a23ca889",
+    "url": "/react-lights-out/static/js/main.13b95454.chunk.js"
+  },
+  {
+    "revision": "cbb5fe1c0ec00797c9e4",
+    "url": "/react-lights-out/static/js/1.cbb5fe1c.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "a0feb5b4dfc2c27ccc9c1737e65b38ff",
+    "url": "/react-lights-out/index.html"
+  }
+];
